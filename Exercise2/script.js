8245169dfc3eafb10b7ff7740a138f0254c72f6d
mathmatowicki <matowicki@gmail.com>
2021-03-11 10:59:09 +0100

    Solve 2 problems from exercise

---
+++ Exercise2/script.js
@@ -0,0 +1,28 @@
+const fizBuz = () => {
+  let out = "";
+  for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0 && i % 3 == 0) out += " fizbuz ";
+    else if (i % 2 == 0) out += " fiz ";
+    else if (i % 3 == 0) out += " buz ";
+    else out += i;
+  }
+  return out;
+};
+
+let circle = (r) => {
+  let out = "";
+  const circumference = r * Math.PI * 2;
+  const area = Math.pow(r, 2) * Math.PI;
+  out =
+    "Circumference of circle is equal = " +
+    circumference +
+    "\n" +
+    "Area pf circle is equal = " +
+    area;
+  return out;
+};
+//Ad1
+console.log(fizBuz());
+//Ad2
+const radius = prompt("Please enter radius of circle", "10");
+console.log(circle(radius));
