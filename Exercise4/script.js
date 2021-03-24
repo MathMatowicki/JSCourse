db8c49252a26aaee750994aa29c8e7fba1173559
mathmatowicki <matowicki@gmail.com>
2021-03-24 12:00:39 +0100

    Add first 2 classess Product and ListOfGoods

--- Exercise4/script.js
+++ Exercise4/script.js
@@ -48,11 +48,90 @@ Sklep zaś będzie zwykłą listą towarów, które można wyśweitlić lub doda
 Na koniec dodaj zamówienie, które będzie umożliwiało dodanie produktów do zamówienia (po ID istniejącego produktu) oraz metodę zrealizujZamowienie, ktora to usuwa produkt odpowiedniej ilości sztuk z magazynu z którego pochodzi. */
 
 class Product {
-    id;
-    
-    constructor()
-}
-class ListofGoods {
+    constructor(id, name, type, price, energyConsumption, yearProduction) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+        this.energyConsumption = energyConsumption;
+        this.yearProduction = yearProduction;
+    }
+    get price() {
+        return this.price;
+    }
+    get costOfEnergy() {
+        const priceOfEnergy = 2.22;
+        return this.energyConsumption * priceOfEnergy;
+    }
 
+    get ageOfProduct() {
+        return new Date().getFullYear() - this.yearProduction;
+    }
+    get ageOfProductYears() {
+        const gap = this.ageOfProduct();
+        if (gap == 1) return gap + " year"
+        return gap + " years";
+    }
+
+    set id(id) {
+        this.id = id;
+    }
+
+    set name(name) {
+        this.name = name;
+    }
+    set type(type) {
+        this.type = type;
+    }
+    set price(price) {
+        this.price = price;
+    }
+    set energyConsumption(energyConsumption) {
+        this.energyConsumption = energyConsumption;
+    }
+    set yearProduction(yearProduction) {
+        this.yearProduction = yearProduction;
+    }
+}
+class ListOfGoods {
+    products = [];
+
+    showProduct(id) {
+        return this.products.filter(Product => Product.id = id);
+    }
+
+    showAllProducts() {
+        return this.products;
+    }
+
+    hasId(product) {
+        return this.products.some(pro => pro.id == product.id) ? true : false;
+    }
+
+    addProduct(product) {
+        if (!hasId) {
+            this.products.push(product);
+        }
+    }
+
+    changeProduct(id, product) {
+        if (this.hasId(product)) {
+            const indexOfProduct = this.products.findIndex((el) => el == id);
+            if (indexOfProduct >= 0) {
+                this.products[indexOfProduct].id = product.id;
+                this.products[indexOfProduct].name = product.name;
+                this.products[indexOfProduct].type = product.type;
+                this.products[indexOfProduct].price = product.price;
+                this.products[indexOfProduct].energyConsumption = product.energyConsumption;
+                this.products[indexOfProduct].yearProduction = product.yearProduction;
+            }
+        }
+    }
+}
+
+class Shop extends ListOfGoods {
+
+}
+class Warehouse extends ListOfGoods {
 
 }
